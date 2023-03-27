9fbae5684fed953cb0ab50de2055e55d19c296b0
Yuna <cleochen93@gmail.com>
2023-03-27 06:28:25 -0700

    Function takes a pointer to an int

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,5 +9,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
