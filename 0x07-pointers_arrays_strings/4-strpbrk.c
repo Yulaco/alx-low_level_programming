132b75f4a5bba345cb0018522ad410bf1d39ed16
Yuna <cleochen93@gmail.com>
2023-04-03 16:45:45 -0700

    Searches for a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,12 +2,23 @@
 
 /**
  * _strpbrk - Function that finds the first occurence in the string.
- * @s:
- * @accept:
+ * @s: String to be located.
+ * @accept: Bytes in the string to be located.
  *
- * Return:n.
+ * Return: 0.
  */
 char *_strpbrk(char *s, char *accept)
 {
+	int string_search;
+
+	while (*s)
+	{
+		for (string_search = 0; accept[string_search]; string_search++)
+		{
+			if (*s == accept[string_search])
+				return (s);
+		}
+		s++;
+	}
 	return (0);
 }
