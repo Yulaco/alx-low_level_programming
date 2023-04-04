c2974095fe35d2097515d7d89fd2940ce90224f9
Yuna <cleochen93@gmail.com>
2023-04-04 15:24:06 -0700

    Finding a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,14 +1,33 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * -strsr - Function that finds the first substring needle,
+ * _strstr - Function that finds the first substring needle,
  *  in the string haystack.
- *  @haystack:
- *  @needle:
+ *  @haystack: longer string to find
+ *  @needle: substring to find for
  *
- *  Return 0.
+ *  Return: a pointer to the beginning of the located substring, or retrun NULL
  */
 char *_strstr(char *haystack, char *needle)
 {
-	Return (0);
+	int i;
+	int s = 0;
+
+	while (needle[s] != '\0')
+		s = s + 1;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i = i + 1)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (i != s)
+			haystack = haystack + 1;
+		else
+			return (haystack);
+	}
+	return (NULL);
 }
