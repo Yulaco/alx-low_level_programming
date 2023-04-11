dc802ac4f9b3a5992754221c8fd562dc844d71d1
Yuna <cleochen93@gmail.com>
2023-04-10 17:21:21 -0700

    Multiplication of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,11 +14,11 @@ __attribute__((unused)) char *argv[])
 	if (argc != 3)
 	{
 		printf("%s\n", "Error");
-		
+
 		return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	
+
 	return (0);
 }
 
