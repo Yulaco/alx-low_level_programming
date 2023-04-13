4f13cecac1e3ced145191a5e95f88b80ac2d58ff
Yuna <cleochen93@gmail.com>
2023-04-13 15:43:27 -0700

    Function asssigns storage for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *cal;
 	unsigned int loc;
 
-	if (nmemb == 0 && size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	cal = malloc(size * nmemb);
