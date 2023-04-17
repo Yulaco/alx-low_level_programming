4d9c3bae6711d6ddb70ac57487f80068fb04a5e8
Yuna <cleochen93@gmail.com>
2023-04-17 09:45:32 -0700

    Header file that defines macro called SIZE

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,18 @@
 #ifndef "0-object_like_macro.h"
 #define "0-object_like_macro.h"
 
+/**
+ * main - Generate a header folder that deines a macro.
+ *
+ * @SIZE: Macro represents a symbolic name for a token.
+ *
+ * Return 0.
+ */
+
+int main(void)
+{
 #define SIZE  1024
+}
 
 #endif
+
