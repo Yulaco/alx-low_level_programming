ded01801aba624c96130df1f571d85876b92f253
Yuna <cleochen93@gmail.com>
2023-04-17 17:39:49 -0700

    New type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
  * struct dog - New data type to join dissimilar kinds of data items.
@@ -22,5 +22,9 @@ typedef struct dog
 #define MAIN_C
 
 void init_dog(struct dog *d, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
