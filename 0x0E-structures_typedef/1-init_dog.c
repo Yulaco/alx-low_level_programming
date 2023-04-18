a909426ec8b249f27b9b059e7e1bbd67250b70bd
Yuna <cleochen93@gmail.com>
2023-04-17 17:31:39 -0700

    Function assigns a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Function that assigns a variable of type struct dog.
+ * 
+ * @d: Pointer directing to data type struct.
+ * @name: Pointer directing to character.
+ * @age: Age of dog indicated as type float.
+ * @owner: Pointer directing to character.
+ *
+ */
+
+void init_dog(struct dog * d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	d = malloc(sizeof(struct dog));
+
+	(*d).name = name;
+	(*d).owner = owner;
+	(*d).age = age;
+}
