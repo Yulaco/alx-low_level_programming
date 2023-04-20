126be68b6a9db54f05222224da46ae8c6a2fa042
Yuna <cleochen93@gmail.com>
2023-04-20 16:50:27 -0700

    Function that prints string

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Function that displays a sequence of characters.
+ *
+ * @*separator:
+ * @n:
+ *
+ * Return 0.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list printStr;
+
+	va_startp(printStr, n);
+}
