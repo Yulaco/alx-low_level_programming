ced451a807f351932a13ff9b3613aa561592d08e
Yuna <cleochen93@gmail.com>
2023-04-27 11:03:54 -0700

    Returns number of elements of list_t

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - The function taht gets back the amount of elements in list.
+ *
+ * @h: Pointer of the list.
+ *
+ * Return: The elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t lst_node = 0;
+
+	while (h == NULL)
+	{
+		h = h->next;
+		lst_node++;
+	}
+
+	return (lst_node);
+}
