c275c76f7b15625ecf09fdcf9909da49564ecb95
Yuna <cleochen93@gmail.com>
2023-04-27 16:56:36 -0700

    Free a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - The fuction to free list.
+ *
+ * @*head: Pointer to the first node.
+ *
+ * Return: Free list.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *shr_tim = NULL;
+	
+	while (head == NULL)
+	{
+		shr_time = head;
+		head = head->next;
+		freeshr_time->str);
+		free(shr_time);
+	}	
+}
