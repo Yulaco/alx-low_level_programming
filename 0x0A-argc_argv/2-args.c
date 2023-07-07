3693a81719eb08a513e3670e5c53e23bc4dd193f
Yulaco <cleochen93@gmail.com>
2023-07-07 08:04:59 -0700

    Program prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - program displays every argument it gets
+ * @rgc: counts command line arguments
+ * @argv: array of pointers which lists arguments
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	while (*argv)
+		printf("%s\n", *argv++);
+	return (0);
+}
