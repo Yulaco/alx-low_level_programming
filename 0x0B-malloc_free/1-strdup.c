59a0b6955f895010e9d37fa67263a62e7a1ad97f
Yulaco <cleochen93@gmail.com>
2023-07-10 14:12:48 -0700

    Returns pointer to a newly allocated memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * *_strdup - gets back a pointer to a new space in 
+ * memory, holds a duplicate of the string given as a parameter
+ * @str: string to be checked
+ *
+ * Return: NULL
+ */
+char *_strdup(char *str)
+{
+	char *addr_var;
+
+	int str_length, a;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (str_length = 0; str[str_length]; str_length++)
+	{
+		addr_var = malloc(sizeof(char) * (str_length +1));
+	}
+
+	if (!addr_var)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < str_length; a++)
+	{
+		addr_var[a] = str[a];
+	}
+	addr_var[a] = '\0';
+	return(addr_var);
+}
