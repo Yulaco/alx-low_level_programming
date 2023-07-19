e3e38e9fd25b2212c421e552a66e51b18be828d8
Yulaco <cleochen93@gmail.com>
2023-07-19 14:15:57 -0700

    Contain function that selects the operation to perform

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - Function selects operations performed by user.
+ *
+ * @s: Pointer to the string.
+ *
+ * Return: Pointer to the function that corresponds to the
+ * operator given as parameter.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+
+	int i;
+
+	for (i = 0; (ops[i].op); i++)
+	{
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
+			return (ops[i].f);
+	}
+	return (NULL);	
+}
