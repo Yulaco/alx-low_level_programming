49cae3d17f463c651ec155613b9283ffa07b786a
Yulaco <cleochen93@gmail.com>
2023-07-28 10:20:16 -0700

    Allocates memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb  == 0 || size == 0)
 		return (NULL);
 
-	alocar = malloc(sizeof(char) * nmemb);
+	alocar = malloc(size * nmemb);
 
 	if (alocar == NULL)
 		return (NULL);
