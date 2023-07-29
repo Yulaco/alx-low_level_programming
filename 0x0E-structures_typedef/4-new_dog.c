d779b3aa631bc524c9a1087e1e949a786f6098c6
Yulaco <cleochen93@gmail.com>
2023-07-29 10:07:59 -0700

    Creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,84 +1,31 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+#include <string.h>
+#include "dog.h"
 
 /**
- * new_dog - Function prints elements accessed from struct.
+ * new_dog - Creates a new dog.
+ * @name: Pointer to name of dog.
+ * @age: Age of dog.
+ * @owner: Pointer to string owner of dog.
  *
- * @name: Name of dog to be copied and stored.
- * @age: Value of dog's age.
- * @owner: Owner copy to be stored and stored.
- *
- * Return: NULL if fails.
+ * Return: NULL if fail.
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *novo;
-		return (NULL);
-
 
 	novo = malloc(sizeof(dog_t));
 
-	if (!novo)
+	if (novo == NULL)
+	{
+		printf("fail\n");
 		return (NULL);
-
-	if (name)
-	{
-		novo->name = _strdup(name);
-		if (!(novo->name))
-		{
-			free(novo);
-			return (NULL);
-		}
 	}
-	else
-		novo->name = NULL;
 
+	novo->name = strdup(name);
 	novo->age = age;
-
-	if (owner)
-	{
-		novo->owner = _strdup(owner);
-		if (!(novo->owner))
-		{
-			free(novo->name);
-			free(novo);
-			return (NULL);
-		}
-	}
-	else
-		novo->owner = NULL;
+	novo->owner = strdup(owner);
 
 	return (novo);
-
-}
-
-/**
- * _strdup -Function copies string.
- *
- * @str: Pointer to the string.
- *
- * Return: 0.
- */
-
-char *_strdup(char *str)
-{
-	char *ptr;
-	unsigned int tamanho = 0;
-
-	if (str == NULL)
-	{
-		for (tamanho = 0; str[tamanho]; tamanho++)
-
-		ptr = malloc(sizeof(char)  * (tamanho + 1);
-
-		if (ptr == NULL)
-		{
-			for (tamanho = 0; ptr[tamanho = str[tamanho]; tamanho--)
-
-			return (ptr);
-		}
-	}
-	return (0);
 }
