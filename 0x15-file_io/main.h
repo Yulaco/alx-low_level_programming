b7a4874b7023702df1edf97d6c431c5ce4b13f9d
Yulaco <cleochen93@gmail.com>
2023-08-07 06:48:45 -0700

    File I/O header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,8 @@
 #define _HEADER_H_
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
