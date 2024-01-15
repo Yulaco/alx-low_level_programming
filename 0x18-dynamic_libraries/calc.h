089bea502cfdb857cbbff68cd7009b941220b9c2
Yulaco <cleochen93@gmail.com>
2024-01-15 13:53:26 -0800

    Calculation header

---
+++ 0x18-dynamic_libraries/calc.h
@@ -0,0 +1,10 @@
+#ifndef CALC_H
+#define CALC_H
+
+int _add(int a, int b);
+int _sub(int a, int b);
+int _mul(int a, int b);
+int _div(int a, int b);
+int _mod(int a, int b);
+
+#endif
---
+++ 0x18-dynamic_libraries/math.h
@@ -0,0 +1,10 @@
+#ifndef MATH_H
+#define MATH_H
+
+int _add(int a, int b);
+int _sub(int a, int b);
+int _mul(int a, int b);
+int _div(int a, int b);
+int _mod(int a, int b);
+
+#endif
